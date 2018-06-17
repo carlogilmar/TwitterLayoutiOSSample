05a337de45077548c1cc4afa17467de585cf3b3c
carlogilmar <carlogilmar12@gmail.com>
2018-06-16 23:35:53 -0500

    Creating 4 cells in the home controller

--- TwitterLBTA/HomeController.swift
+++ TwitterLBTA/HomeController.swift
@@ -9,9 +9,38 @@
 import UIKit
 
 class HomeController: UICollectionViewController {
+    
+    /*
+    - Controller and their cycle life
+    - Collection View object and function
+        - numberOfItemsInSection
+        - cellForItemAt
+        - dequeueReusableCell
+        - Register
+    - UI Collection View Controller Object
+    - UIKit library
+     */
+    
+    let cellId = "cellId"
+    
+    // Load the controller
     override func viewDidLoad(){
         super.viewDidLoad()
         print("Step 2: I'm the HomeController!")
         collectionView?.backgroundColor = .green
+        // Register the cells
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
+    
+    // Make the cells
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
 }
