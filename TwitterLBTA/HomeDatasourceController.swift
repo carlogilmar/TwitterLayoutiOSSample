515e02b370381106ce257eae80fdc49342979d08
carlogilmar <carlogilmar12@gmail.com>
2018-06-18 23:34:18 -0500

    Update HomeDatasourceController

--- TwitterLBTA/HomeDatasourceController.swift
+++ TwitterLBTA/HomeDatasourceController.swift
@@ -14,6 +14,7 @@ class HomeDatasourceController: DatasourceController {
      */
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .red
+        //collectionView?.backgroundColor = .red
     }
 }
+
