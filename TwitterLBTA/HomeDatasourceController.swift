98182a8e2ac275e5575c7a557fe2b853a1568266
carlogilmar <carlogilmar12@gmail.com>
2018-06-18 23:57:46 -0500

    Adding UserCell class in HomeDatasourceController

--- TwitterLBTA/HomeDatasourceController.swift
+++ TwitterLBTA/HomeDatasourceController.swift
@@ -12,9 +12,30 @@ import LBTAComponents
     Creating a new swift class for provide a simple list
  */
 
+class UserCell: DatasourceCell {
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "helloooo!!!"
+        return label
+    }()
+    
+    override func setupViews() {
+        print("UserCell setup views!")
+        super.setupViews()
+        backgroundColor = .yellow
+        addSubview(nameLabel)
+        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+    }
+}
+
 class HomeDatasource: Datasource {
     
     let words = ["user1", "user2", "user3"]
+    
+    override func cellClasses() -> [DatasourceCell.Type] {
+        print(" cellClasses() ==> ")
+        return [UserCell.self]
+    }
     
     override func item(_ indexPath: IndexPath) -> Any? {
         print("Function item ---------- ")
