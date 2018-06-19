045ec8fb94141b6c3918229ba57e37fd4a212848
carlogilmar <carlogilmar12@gmail.com>
2018-06-18 23:50:08 -0500

    Adding HomeDatasource class

--- TwitterLBTA/HomeDatasourceController.swift
+++ TwitterLBTA/HomeDatasourceController.swift
@@ -8,6 +8,29 @@
 
 import LBTAComponents
 
+/*
+    Creating a new swift class for provide a simple list
+ */
+
+class HomeDatasource: Datasource {
+    
+    let words = ["user1", "user2", "user3"]
+    
+    override func item(_ indexPath: IndexPath) -> Any? {
+        print("Function item ---------- ")
+        print(indexPath.item)
+        print(words[indexPath.item])
+        print("--------------------")
+        return words[indexPath.item]
+    }
+    
+    override func numberOfItems(_ section: Int) -> Int {
+        print("Number of items!!")
+        return words.count
+    }
+    
+}
+
 class HomeDatasourceController: DatasourceController {
     /*
      Datasource controller is a LBTA Component
@@ -15,6 +38,11 @@ class HomeDatasourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //collectionView?.backgroundColor = .red
+        let homeDatasource = HomeDatasource()
+        print("HomedatasourceController is here!!")
+        print(homeDatasource.description)
+        print("==================================")
+        self.datasource = homeDatasource
     }
 }
 
