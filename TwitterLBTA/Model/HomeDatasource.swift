101c9a04f884decdd6f126306689106c4a054ef6
carlogilmar <carlogilmar12@gmail.com>
2018-06-19 00:24:49 -0500

    Implement MVC pattern

--- TwitterLBTA/Model/HomeDatasource.swift
+++ TwitterLBTA/Model/HomeDatasource.swift
@@ -5,5 +5,36 @@
 //  Created by Carlo Gilmar on 19/06/18.
 //  Copyright © 2018 Carlo Gilmar. All rights reserved.
 //
+import LBTAComponents
 
-import Foundation
+class HomeDatasource: Datasource {
+    
+    let words = ["user1", "user2", "user3"]
+    
+    override func cellClasses() -> [DatasourceCell.Type] {
+        print(" cellClasses() ==> ")
+        return [UserCell.self]
+    }
+    
+    override func item(_ indexPath: IndexPath) -> Any? {
+        print("Function item ---------- ")
+        print(indexPath.item)
+        print(words[indexPath.item])
+        print("--------------------")
+        return words[indexPath.item]
+    }
+    
+    override func numberOfItems(_ section: Int) -> Int {
+        print("Number of items!!")
+        return words.count
+    }
+    
+    override func headerClasses() -> [DatasourceCell.Type]? {
+        return [UserHeader.self]
+    }
+    
+    override func footerClasses() -> [DatasourceCell.Type]? {
+        return [UserFooter.self]
+    }
+    
+}
--- TwitterLBTA/View/Cells.swift
+++ TwitterLBTA/View/Cells.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2018 Carlo Gilmar. All rights reserved.
 //
 
-import Foundation
+import LBTAComponents
+
+
+/*
+ Creating a new swift class for provide a simple list
+ */
+
+class UserHeader: DatasourceCell {
+    override func setupViews() {
+        super.setupViews()
+        backgroundColor = .blue
+    }
+}
+
+class UserFooter: DatasourceCell{
+    override func setupViews() {
+        super.setupViews()
+        backgroundColor = .green
+    }
+}
+
+class UserCell: DatasourceCell {
+    
+    override var datasourceItem: Any? {
+        didSet{
+            print("Did set :-D ------------------------------------>")
+            nameLabel.text = datasourceItem as? String
+        }
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "helloooo!!!"
+        return label
+    }()
+    
+    override func setupViews() {
+        print("UserCell setup views!")
+        super.setupViews()
+        backgroundColor = .yellow
+        addSubview(nameLabel)
+        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+    }
+}
