79a2d1b61b2cf50ce34813f3458eedd8034b7120
carlogilmar <carlogilmar12@gmail.com>
2018-06-21 00:44:58 -0500

    Adding profile image red square in the view

--- TwitterLBTA/Controller/HomeDatasourceController.swift
+++ TwitterLBTA/Controller/HomeDatasourceController.swift
@@ -22,6 +22,11 @@ class HomeDatasourceController: DatasourceController {
         self.datasource = homeDatasource
     }
     
+    // image profile square
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
