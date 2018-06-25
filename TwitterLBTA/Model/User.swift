6b2296d2c6360b9a37df88bda7e625e9562fe4f4
carlogilmar <carlogilmar12@gmail.com>
2018-06-25 00:17:27 -0500

    Adding user file

---
+++ TwitterLBTA/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  TwitterLBTA
+//
+//  Created by Carlo Gilmar on 25/06/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name:String
+    let username:String
+    let bioText:String
+}
