dd39a5db37c839a6ad51a7de8db07d95bed26a6c
carlogilmar <carlogilmar12@gmail.com>
2018-06-29 00:15:56 -0500

    Adding tweet cell for show the second section

---
+++ TwitterLBTA/View/TweetCell.swift
@@ -0,0 +1,18 @@
+//
+//  TweetCell.swift
+//  TwitterLBTA
+//
+//  Created by Carlo Gilmar on 29/06/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import LBTAComponents
+
+class TweetCell: DatasourceCell {
+    
+    override func setupViews() {
+        super.setupViews()
+        backgroundColor = .red
+    }
+    
+}
