5db780ed23cf92e909fd19953e6a7cd7b1b8629d
carlogilmar <carlogilmar12@gmail.com>
2018-06-29 00:22:33 -0500

    Adding profile image in every tweetCell

--- TwitterLBTA/View/TweetCell.swift
+++ TwitterLBTA/View/TweetCell.swift
@@ -10,11 +10,32 @@ import LBTAComponents
 
 class TweetCell: DatasourceCell {
     
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        //imageView.backgroundColor = .red
+        imageView.image = #imageLiteral(resourceName: "profile")
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        return imageView
+    }() // Closure properties as Computed properties
+    
     override func setupViews() {
         super.setupViews()
         separatorLineView.isHidden = false
         separatorLineView.backgroundColor = UIColor(r: 230, g: 230, b: 230)
         backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(self.topAnchor,
+                                left: self.leftAnchor,
+                                bottom: nil,
+                                right: nil,
+                                topConstant: 12,
+                                leftConstant: 12,
+                                bottomConstant: 0,
+                                rightConstant: 0,
+                                widthConstant: 50,
+                                heightConstant: 50)
     }
     
 }
