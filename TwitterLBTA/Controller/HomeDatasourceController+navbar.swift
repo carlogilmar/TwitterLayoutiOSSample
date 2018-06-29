426cbc4b838253b4e527c5fa49ff9d1c5f645b67
carlogilmar <carlogilmar12@gmail.com>
2018-06-29 00:30:49 -0500

    Modify nav navigation Bar

--- TwitterLBTA/Controller/HomeDatasourceController+navbar.swift
+++ TwitterLBTA/Controller/HomeDatasourceController+navbar.swift
@@ -21,6 +21,16 @@ extension HomeDatasourceController{
         titleImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
         titleImageView.contentMode = .scaleAspectFit
         navigationItem.titleView = titleImageView
+        
+        navigationController?.navigationBar.backgroundColor = .red
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        
+        let navBarSeparatorView = UIView()
+        navBarSeparatorView.backgroundColor = UIColor(r: 230, g: 230, b: 230)
+        view.addSubview(navBarSeparatorView)
+        navBarSeparatorView.anchor(view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0.5)
     }
     
     private func setupLeftNavItem(){
